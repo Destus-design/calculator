0c02e7f1f971ee46ab0b31188d86b341fdc263c0
nnekasandra <nnekasandra2016@gmail.com>
2020-11-05 21:49:25 +0100

    commit after merging and fixing issues in scripts.js file

--- scripts.js
+++ scripts.js
@@ -54,8 +54,22 @@ class calculator{
         this.previousOperand = ''
     }
     getDisplayNumber(number){
-        
-        return number
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay;
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        }else{
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else{
+            return integerDisplay
+        }
     }
     updateDisplay(){
         this.currentOperandTextElement.innerText = 
@@ -63,6 +77,8 @@ class calculator{
         if(this.operation != null){
         this.previousOperandTextElement.innerText =
         `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }else{
+        this.previousOperandTextElement.innerText = ''
     }
 }
 
